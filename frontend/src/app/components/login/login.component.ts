fac41430deda5a9696f8bacace993cc1f9134273
Gaál Gergely <gaalgergely@yahoo.com>
2019-08-31 00:02:56 +0200

    login form is added (CORS fail)

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form = {
+    email: null,
+    password: null
+  };
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    return this.http.post('http://api.angular-auth.laravel/api/login', this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
   }
 
 }
