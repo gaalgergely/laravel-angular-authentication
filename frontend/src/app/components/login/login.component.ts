9264856cbdabaedb249759656d5b47570a9f2336
Gaál Gergely <gaalgergely@yahoo.com>
2019-09-01 14:10:37 +0200

    login/logout work on frontend

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {JarwisService} from '../../services/jarwis.service';
 import {TokenService} from '../../services/token.service';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +20,9 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private Jarwis: JarwisService,
-    private Token: TokenService
+    private Token: TokenService,
+    private router: Router,
+    private Auth: AuthService
   ) { }
 
   ngOnInit() {
@@ -33,6 +37,8 @@ export class LoginComponent implements OnInit {
 
   handleResponse(data) {
     this.Token.handle(data.access_token);
+    this.Auth.changeAuthStatus(true);
+    return this.router.navigateByUrl('/profile');
   }
 
   handleError(error) {
